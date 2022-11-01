1cc93a03f6e4030f5e2bab23a8300bafd2c20a6a
Emmanuel <79337127+CarlosErz@users.noreply.github.com>
2022-11-01 16:50:16 -0600

    Add files via upload

---
+++ js/animaciones.js
@@ -0,0 +1,15 @@
+window.sr = ScrollReveal({ reset: true })
+sr.reveal('.nombre',
+ { duration: 1000,
+  origin: 'right',
+
+});
+sr.reveal('.flor', {
+  rotate: { x: 20, y: 80, z: 0 },
+duration: 1000,
+});
+sr.reveal('.ada',{
+  origin: 'left',
+  delay:300,
+})
+
